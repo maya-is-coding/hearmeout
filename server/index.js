81a218125f3e214febe44c00111a5545cdf13098
Anusree <anusreemoolayil@gmail.com>
2026-05-15 18:08:52 +0530

    fix cors for vercel frontend

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // 3. Attach socket.io to the http server
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "https://hearmeout-seven.vercel.app"],
         methods: ["GET", "POST"]
     }
 });
